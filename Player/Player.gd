251ec0d25e7d11365773c1e41ff25014920d5e4b
julianahand <77911991+julianahand@users.noreply.github.com>
2021-02-20 00:40:05 -0500

    Complete Exercise

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,16 @@
 extends KinematicBody2D
 
+onready var HUD = get_node("/root/Game/HUD")
 export var speed = 7
 
 
 func _physics_process(delta):
 	position += get_input()*speed
 	if Input.is_action_pressed("shoot") and not $Laser.is_casting:
-		$Laser.fire(Vector2(global_position.x, 0))
+		$Laser.fire(Vector2(global_position.x,0))
 	elif not Input.is_action_pressed("shoot") and $Laser.is_casting:
 		$Laser.stop()
-	
+   
 
 
 func get_input():
@@ -22,4 +23,5 @@ func get_input():
 
 
 func _on_Damage_body_entered(body):
+	HUD.update_health(-body.damage)
 	body.die()
